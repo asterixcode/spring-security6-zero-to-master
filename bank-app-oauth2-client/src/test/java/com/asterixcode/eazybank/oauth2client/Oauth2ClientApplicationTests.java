bcee61bb78722238653ecf6a16671807ff6d38d1
lucas.saldanha <lucas.saldanha@bestseller.com>
2025-03-30 09:18:22 +0200

    refactor: remove tests

--- bank-app-oauth2-client/src/test/java/com/asterixcode/eazybank/oauth2client/Oauth2ClientApplicationTests.java
+++ bank-app-oauth2-client/src/test/java/com/asterixcode/eazybank/oauth2client/Oauth2ClientApplicationTests.java
@@ -1,13 +1,6 @@
 package com.asterixcode.eazybank.oauth2client;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class Oauth2ClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  void contextLoads() {}
 }
